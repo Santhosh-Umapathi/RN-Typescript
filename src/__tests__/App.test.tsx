8d285e110bd719dcd8afc5f4c7bd5a9e863b1616
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2022-07-25 11:56:53 +0200

    CHORE: Tests added for component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../../App';
+
 function add(a: number, b: number) {
   return a + b;
 }
 
-//Test a simple function
 describe('App', () => {
+  //Test a simple function
   test('should return number', () => {
     expect(add(2, 2)).toBe(4);
+  });
+
+  //Test a simple React Component
+  test('should render correctly', () => {
+    renderer.create(<App />);
   });
 });
